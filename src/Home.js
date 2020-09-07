d39e5cf5b1fb4f7e155b7c09f75103dd229749fa
Tracy Nguyen <traciinguyen@hotmail.com>
2020-09-07 12:47:34 -0400

    Completed header

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Navbar from './Navbar';
+import './styles/Home.css';
 
 
 const Home = () => {
     return (
+        <div>
         <Navbar/>
+        <div className="header">
+            <h2>Unlimited is Over Crowded</h2>
+            <p>The word is real content during design can distract designers and design review 
+                teams alike away from the design, and insisting on always.</p>
+                <input type="button" value="Hire Us Now"/>
+                <img src={require('./images/header-rocket.jpg')}/>
+        </div>
+        </div>
     );
 };
 
